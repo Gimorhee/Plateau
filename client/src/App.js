2d722dd3e417ae15f6430fb4b5024140f21a8823
Danny Rhee <dongyunrhee@gmail.com>
2019-10-14 20:01:08 -0700

    Created Redux store

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,26 @@ import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-        <Footer />
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+          <Footer />
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
