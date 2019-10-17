1c4d828ca560af76e74f0b649ee4e2ce985a489b
Danny Rhee <dongyunrhee@gmail.com>
2019-10-16 22:09:31 -0700

    Navbar links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,66 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
 import "../../css/navbar.css";
 
-const Navibar = props => {
+const Navibar = ({ auth: { isAuthenticated, loading } }) => {
+  const guestUser = (
+    <Fragment>
+      <NavItem>
+        <NavLink className="Nav-Item" href="/register">
+          Register
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="Nav-Item" href="/login">
+          Login
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="Nav-Item" href="/cart">
+          My Cart
+        </NavLink>
+      </NavItem>
+    </Fragment>
+  );
+
+  const authUser = (
+    <Fragment>
+      <NavItem>
+        <NavLink className="Nav-Item" href="/landing">
+          Logout
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink className="Nav-Item" href="/cart">
+          My Cart
+        </NavLink>
+      </NavItem>
+    </Fragment>
+  );
+
   return (
     <Navbar className="Nav" light expand="md">
-      <NavbarBrand className="Nav-Brand" href="/">Plateau</NavbarBrand>
+      <NavbarBrand className="Nav-Brand" href="/">
+        Plateau
+      </NavbarBrand>
       <Nav className="ml-auto" navbar>
-        <NavItem>
-          <NavLink className="Nav-Item" href="/register">Register</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="Nav-Item" href="/login">Login</NavLink>
-        </NavItem>
+        {!loading && (
+          <Fragment>{isAuthenticated ? authUser : guestUser}</Fragment>
+        )}
       </Nav>
     </Navbar>
   );
 };
 
-export default Navibar;
+Navibar.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Navibar);
