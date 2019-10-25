d2df9aee48852c641f2ea04c81a38b5a20350dae
Danny Rhee <dongyunrhee@gmail.com>
2019-10-24 18:30:13 -0700

    MyCart model

---
+++ models/MyCart.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MyCartSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    items: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            },
+            size: {
+                type: String,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+});
+
+module.exports = MyCart = mongoose.model("myCart", MyCartSchema);
+
