4839a3fc9205310fb1654bb7068b97fa266b0bf8
Danny Rhee <dongyunrhee@gmail.com>
2019-11-07 21:36:36 -0800

    Handled empty cart / Created Checkout component

---
+++ client/src/components/myCart/Checkout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import "../../css/mycart.css";
+
+const Checkout = props => {
+    return (
+        <div>
+            <h2 className="Header">SHOPPING BAG TOTAL</h2>
+        </div>
+    )
+}
+
+Checkout.propTypes = {
+
+}
+
+export default Checkout
--- client/src/components/myCart/MyCart.js
+++ client/src/components/myCart/MyCart.js
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getMyCartItems } from "../../actions/myCart";
 import Spinner from "../layout/Spinner";
+import Checkout from "./Checkout";
+
+import "../../css/mycart.css";
 
 const MyCart = ({ myCart: { items, loading }, getMyCartItems }) => {
   useEffect(() => {
@@ -11,14 +14,30 @@ const MyCart = ({ myCart: { items, loading }, getMyCartItems }) => {
 
   return loading || items.items === null ? (
     <Spinner />
-  ) : items.items ? (
-    <Fragment>
-      {items.items.map(item => (
-        <div key={item._id}>{item.name}</div>
-      ))}
-    </Fragment>
   ) : (
-    <p>No items in your Cart</p>
+    <Fragment>
+      <h1 className="MyCart-Header">SHOPPING BAG</h1>
+      <div className="MyCart-Container">
+        {items.items.length > 0 ? (
+          <div>
+            {items.items.map(item => (
+              <div key={item._id}>{item.name}</div>
+            ))}
+          </div>
+        ) : (
+          <div className="Empty-Cart">
+            <h2 className="Header">YOUR SHOPPING BAG IS EMPTY!</h2>
+            <p className="Small">
+              Checkout our new arrivals and add them in your shopping bag!
+            </p>
+            <a href="/" className="Back-Button">
+              Back
+            </a>
+          </div>
+        )}
+        <Checkout />
+      </div>
+    </Fragment>
   );
 };
 
