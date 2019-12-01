c35a0bfdf59a985870e38465bb9c8fddc80a2432
Danny Rhee <dongyunrhee@gmail.com>
2019-11-30 21:45:45 -0800

    Payment Info Form

--- client/src/components/payment/CustomerInfo.js
+++ client/src/components/payment/CustomerInfo.js
@@ -4,8 +4,8 @@ const CustomerInfo = ({ user }) => {
     return (
         <div className="Info-Container">
             <h3 className="Info-Header"><strong>MY INFORMATION</strong></h3>
-            <h5>Name: {user && user.firstName} </h5>
-            <h5>Email: {user && user.email } </h5>
+            <h5><strong>Name:</strong> {user && user.firstName} {user && user.lastName} </h5>
+            <h5><strong>Email:</strong> {user && user.email } </h5>
         </div>
     )
 }
--- client/src/components/payment/PaymentInfo.js
+++ client/src/components/payment/PaymentInfo.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const PaymentInfo = props => {
     return (
-        <div>
-            Payment Info
-        </div>
+        <Fragment>
+            <div className="Payment-Info-Container">
+                <h3 className="Payment-Info-Header">Payment Information</h3>
+                <form className="Payment-Info-Form">
+                    <div className="form-group col-md-12">
+                        <label className="Payment-Info-Label"><strong>Card Holder Name</strong></label>
+                        <input type="text" className="form-control" placeholder="Your name on the card"/>
+                    </div>
+                    <div className="form-group col-md-12">
+                        <label className="Payment-Info-Label"><strong>Card Number</strong></label>
+                        <input type="text" className="form-control" placeholder="Your credit card number"/>
+                    </div>
+                    <div className="Payment-Extra-Info">
+                        <div className="form-group col-md-4">
+                            <label className="Payment-Info-Label"><strong>Expiration Date</strong></label>
+                            <input type="text" className="form-control" placeholder="MM/YYYY" />
+                        </div>
+                        <div className="form-group col-md-4">
+                            <label className="Payment-Info-Label"><strong>CVV*</strong></label>
+                            <input type="text" className="form-control" placeholder="" />
+                        </div>
+                        <button className="Delivery-Button">Make a Payment</button>
+                    </div>
+                </form>
+            </div>
+        </Fragment>
     )
 }
 
