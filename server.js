bcc75928ac66503b88a02f235dae81a9fca71174
Danny Rhee <dongyunrhee@gmail.com>
2020-03-28 21:47:20 -0700

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 
 // Connect DB
 connectDB();
 
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.send("HOME");
-  console.log("HOME");
-});
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
@@ -19,6 +15,14 @@ app.use("/api/myCart", require("./routes/api/myCart"));
 app.use("/api/delivery", require("./routes/api/deliveryInfo"));
 app.use("/api/payment", require("./routes/api/payment"));
 app.use("/api/order", require("./routes/api/order"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
